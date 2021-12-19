801542606add49a43b281af71ee48f2ec4b80e74
SoMiReMiReDo <xc15242508752@163.com>
2021-12-19 14:44:39 +0800

    qiu qiu ni le!

--- myweb/myweb/settings.py
+++ myweb/myweb/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.static',
+    'django.contrib.staticfiles',
     'myapp',
     'article',
     'userprofile',
