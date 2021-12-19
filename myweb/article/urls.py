51c86ae8d2ba128e1bc8519ffeb4d1ee45a6e9d2
SoMiReMiReDo <xc15242508752@163.com>
2021-12-19 18:48:52 +0800

    完结撒花❀

--- myweb/article/urls.py
+++ myweb/article/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('article_list/', views.article_list, name='article_list'),
     path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
     path('article-create/', views.article_create, name='article_create'),
-    # path('article-delete/<int:id>/', views.article_delete, name='article_delete'),
+    path('article-delete/<int:id>/', views.article_delete, name='article_delete'),
     path(
         'article-safe-delete/<int:id>/',
         views.article_safe_delete,
