cb955d18094014802abe793cd886933920f1e41f
SoMiReMiReDo <xc15242508752@163.com>
2021-12-19 16:17:43 +0800

    q

--- myweb/myweb/urls.py
+++ myweb/myweb/urls.py
@@ -38,4 +38,4 @@ urlpatterns = [
       {'document_root': settings.STATIC_ROOT}, name='static'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
